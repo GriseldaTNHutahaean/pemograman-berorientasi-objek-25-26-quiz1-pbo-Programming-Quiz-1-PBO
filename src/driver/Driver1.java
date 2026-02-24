96cec4a767784e32c0e75c02be8b20ab735246e1
GriseldaTNHutahaean <iss24026@students.del.ac.id>
2026-02-24 14:03:20 +0700

    Update Driver1.java

--- src/driver/Driver1.java
+++ src/driver/Driver1.java
@@ -1,5 +1,5 @@
 import java.util.Scanner;
-
+//
 public class Driver1 {
 
     public static void main(String[] args) {
@@ -107,3 +107,4 @@ public class Driver1 {
         input.close();
     }
 }
+
